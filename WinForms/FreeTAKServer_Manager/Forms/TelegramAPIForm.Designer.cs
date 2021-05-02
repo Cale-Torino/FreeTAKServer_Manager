fd158900fb603c17f0902983ede8347f4b3bd6c0
Cale Torino <cale@techrad.co.za>
2021-05-02 14:55:56 +0200

    added files

---
+++ WinForms/FreeTAKServer_Manager/Forms/TelegramAPIForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace FreeTAKServer_Manager
+{
+    partial class TelegramAPIForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelegramAPIForm));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Test_button = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 284);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(952, 298);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Test_button);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(952, 266);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controls";
+            // 
+            // Test_button
+            // 
+            this.Test_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Test_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(37)))));
+            this.Test_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Test_button.ForeColor = System.Drawing.Color.White;
+            this.Test_button.Location = new System.Drawing.Point(41, 25);
+            this.Test_button.Name = "Test_button";
+            this.Test_button.Size = new System.Drawing.Size(157, 34);
+            this.Test_button.TabIndex = 11;
+            this.Test_button.Text = "Test Bot";
+            this.Test_button.UseVisualStyleBackColor = true;
+            // 
+            // TelegramAPIForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(41)))), ((int)(((byte)(61)))));
+            this.ClientSize = new System.Drawing.Size(976, 594);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TelegramAPIForm";
+            this.Text = "Telegram API Form";
+            this.Load += new System.EventHandler(this.TelegramAPIForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Test_button;
+    }
+}
---
+++ WinForms/FreeTAKServer_Manager/Forms/TelegramAPIForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FreeTAKServer_Manager
+{
+    public partial class TelegramAPIForm : Form
+    {
+        public TelegramAPIForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TelegramAPIForm_Load(object sender, EventArgs e)
+        {
+            // 1726488859:AAGy13koBigjBU0h_MSxzglmaAAbPzqH8DI
+
+
+
+        }
+    }
+}

bcf7266f36cc20befa5e7a2e1ea18f0d7b84666a
Cale-Torino <cale@techrad.co.za>
2022-03-13 15:51:14 +0200

    Added secret key to .yaml; checked configs; installs version 1.9.8.6

--- WinForms/FreeTAKServer_Manager/oldbatchmethod/FTS_INSTALL.bat
+++ WinForms/FreeTAKServer_Manager/oldbatchmethod/FTS_INSTALL.bat
@@ -1,5 +1,5 @@
 ::UNINSTALL
 ECHO ON
-:: 1.9.6.1;1.9.7
-start cmd /k pip install -r requirements.txt&&python -m pip install FreeTAKServer[ui]==1.9.7
+:: 1.9.6.1;1.9.7;1.9.8.6
+start cmd /k pip install -r requirements.txt&&python -m pip install FreeTAKServer[ui]==1.9.8.6
 pause

2809308a18dcf714c0219ecd9589a37af440914c
Cale-Torino <cale@techrad.co.za>
2022-07-26 15:33:20 +0200

    Added DelBinObjFolders helper

---
+++ HelperApps/DelBinObjFolders/DelBinObjFolders/Classes/DelClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace DelBinObjFolders
+{
+    internal class DelClass
+    {
+        public static void DelAllFilesAndFolder(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine($"Scanning...");
+                DirectoryInfo directory = new DirectoryInfo(path);
+                foreach (FileInfo file in directory.EnumerateFiles())
+                {
+                    Console.WriteLine($"Deleted {Path.GetFileName(file.Name)}");
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in directory.EnumerateDirectories())
+                {
+                    Console.WriteLine($"Deleted {Path.GetFileName(dir.Name)}");
+                    dir.Delete(true);
+                }
+            }
+            else 
+            {
+                Console.WriteLine($"Directory Does Not Exist");
+            }
+        }
+
+        public static void ListAllFolders(string basepath)
+        {
+            string[] folders = Directory.GetDirectories(basepath, "*", SearchOption.AllDirectories);
+            foreach (string folder in folders)
+            {
+                Console.WriteLine($"{folder}");
+            }
+        }
+
+        public static void GoUp2Dir()
+        {
+            string result = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\"));
+            //var upTwoDir = new DirectoryInfo(Directory.GetCurrentDirectory());
+            //var result = x.Parent.Parent;
+            Console.WriteLine($"{result}");
+        }
+    }
+}
---
+++ HelperApps/DelBinObjFolders/DelBinObjFolders/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DelBinObjFolders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+            Console.WriteLine(AppDomain.CurrentDomain.FriendlyName);
+            Console.WriteLine($"Parameter count = {args.Length}");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Arg[{i}] = [{args[i]}]");
+            }
+            Console.WriteLine("----------------------------------------------");
+            if (args.Length > 0)
+            {
+                Callmethods(args[0]);
+                Console.WriteLine("Delete Files Method Complete");
+                Console.Beep();
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Delete Files Failed Please Add Switch");
+                Console.Beep();
+                Console.ReadKey();
+            }
+        }
+        public static void Callmethods(string arg)
+        {
+            if (arg == "-Run")
+            {
+                //DelClass.ListAllFolders($@"{AppDomain.CurrentDomain.BaseDirectory}");
+                DelClass.DelAllFilesAndFolder($@"{AppDomain.CurrentDomain.BaseDirectory}WinForms\FreeTAKServer_Manager\bin\");
+                DelClass.DelAllFilesAndFolder($@"{AppDomain.CurrentDomain.BaseDirectory}WinForms\FreeTAKServer_Manager\obj\");
+                DelClass.DelAllFilesAndFolder($@"{AppDomain.CurrentDomain.BaseDirectory}WPF\FreeTAKServer_Manager_WPF\bin\");
+                DelClass.DelAllFilesAndFolder($@"{AppDomain.CurrentDomain.BaseDirectory}WPF\FreeTAKServer_Manager_WPF\obj\");
+            }
+            else 
+            {
+                Console.WriteLine("Invalid Arg");
+            }
+        }
+    }
+}
